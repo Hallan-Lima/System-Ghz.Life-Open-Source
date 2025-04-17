d377c43f456193f2e39a5bd54803ec7298a049db
Hallan-Lima <hallansrl.t@gmail.com>
2025-04-17 08:38:43 -0300

    0.0.5 - reestruturando projeto

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+// filepath: p:\xampp\htdocs\www\Projeto\public\index.php
+
+// Define o caminho base para os arquivos estáticos
+define('BASE_URL', '../public/');
+
+// Obtém o caminho da URL
+$request = $_SERVER['REQUEST_URI'];
+
+// Remove a barra inicial e a query string para evitar problemas de correspondência
+$request = explode('?', ltrim($request, '/'))[0];
+
+// Encontra a posição de '/Projeto/' na URL
+$ProjetoPos = strpos($request, '/Projeto/');
+
+// Remove a parte variável da URL, mantendo apenas o caminho após '/Projeto/'
+$request = substr($request, $ProjetoPos+strlen('/Projeto/public/'));
+
+$routers = [
+    'home' => '../src/Views/pages/home.php',
+    'login' => '../src/Views/pages/login.php',
+];
+
+if (array_key_exists($request, $routers)) {
+    require_once '../src/Views/layouts/header.php';
+    require $routers[$request];
+    require_once '../src/Views/layouts/footer.php';
+} else {
+    require_once '../src/Views/layouts/header.php';
+    require '../src/Views/pages/404.php';
+    require_once '../src/Views/layouts/footer.php';
+}
+
+
+
---
+++ src/Views/pages/404.php
@@ -0,0 +1,16 @@
+<div class="container-xxl container-p-y">
+    <div class="misc-wrapper">
+        <h2 class="mb-2 mx-2">Pagina não encontrada</h2>
+        <p class="mb-4 mx-2">Ops! 😖 A URL solicitada não foi encontrada neste servidor.</p>
+        <a href="login" class="btn btn-primary">Voltar</a>
+        <div class="mt-3">
+            <img
+                src="assets/img/illustrations/page-misc-error-light.png"
+                alt="page-misc-error-light"
+                width="500"
+                class="img-fluid"
+                data-app-dark-img="illustrations/page-misc-error-dark.png"
+                data-app-light-img="illustrations/page-misc-error-light.png" />
+        </div>
+    </div>
+</div>
